85d7ec9952c9e80489899495720e3cab42f28199
PavelMakarov1 <166589393+PavelMakarov1@users.noreply.github.com>
2024-05-06 00:19:14 +0200

    add lesson76

---
+++ lesson76/main.js
@@ -0,0 +1,10 @@
+const firstName = 'Pavel'
+const lastName = 'Makarov'
+let age = '29'
+let residence = 'Belarus'
+const learning  = 'Team Aroken'
+
+console.log('My name is', firstName, lastName )
+console.log('I am', age, 'years old.')
+console.log('I live in', residence)
+console.log('I am studying', learning)
