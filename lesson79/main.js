c688c6e1e5bd0e85894a9a7c2fe949b05dbbc872
PavelMakarov1 <166589393+PavelMakarov1@users.noreply.github.com>
2024-05-08 20:09:47 +0200

    add lesson79

---
+++ lesson79/main.js
@@ -0,0 +1,17 @@
+let age = 29;
+let number = 18
+const adult = 'Вы совершеннолетний'
+const noAdult = 'Вам еще нет 18'
+
+// if (age >= number) {
+//     console.log(adult);
+
+// } else {
+//     console.log(noAdult);
+// }
+
+const minors = (age >= number) ? adult: noAdult; console.log(minors)
+
+
+
+
