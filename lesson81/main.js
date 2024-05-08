d8a509d0a302f4687e322404b126c4b63ca626fb
PavelMakarov1 <166589393+PavelMakarov1@users.noreply.github.com>
2024-05-08 20:10:33 +0200

    add lesson81

---
+++ lesson81/main.js
@@ -0,0 +1,11 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// for (let i = 0; i < numbers.length; i++) {
+
+//     if (numbers[i] % 2 === 0)
+//         console.log(numbers[i]);
+// }
+const rainboulColars = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиалетовый'];
+for (let i = rainboulColars.length - 1; i >= 0; i--) {
+    console.log(rainboulColars[i])
+}
